6a4c4ff6b395b52cda296fa65b3979efd9079d70
ooo <oea0805@gmail.com>
2019-10-24 17:02:43 +0900

    20191024| telegram chatbot 활용하기

--- send_message.py
+++ send_message.py
@@ -4,6 +4,6 @@ from decouple import config
 api_url = 'https://api.telegram.org'
 token = config('TELEGRAM_BOT_TOKEN')
 chat_id=config('CHAT_ID')
-text = input('메시지를 입력해주세요: ')
+text = input('INPUT MSG : ')
 
 requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
